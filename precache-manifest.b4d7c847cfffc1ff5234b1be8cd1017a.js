213f6bdcda79dd5ab8998c6a0e42c04a0d1820df
Ken <z121356777@gmail.com>
2019-12-21 11:23:15 +0800

    deploy

---
+++ precache-manifest.b4d7c847cfffc1ff5234b1be8cd1017a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b297849fc3af722627929c08b31630c2",
+    "url": "/KKBOX-Sample/696c7602-4df7-c743-d4cb-76f74fbae76d_620.png"
+  },
+  {
+    "revision": "74b0de4adac3d91299ae",
+    "url": "/KKBOX-Sample/css/app.57a39a49.css"
+  },
+  {
+    "revision": "552c78a0b45873560840",
+    "url": "/KKBOX-Sample/css/chunk-vendors.7e449d15.css"
+  },
+  {
+    "revision": "e26d66b53eea3d452819ad3ae7094e65",
+    "url": "/KKBOX-Sample/index.html"
+  },
+  {
+    "revision": "74b0de4adac3d91299ae",
+    "url": "/KKBOX-Sample/js/app.9ed488a4.js"
+  },
+  {
+    "revision": "552c78a0b45873560840",
+    "url": "/KKBOX-Sample/js/chunk-vendors.9029eee5.js"
+  },
+  {
+    "revision": "79e198297859b30d1389d35433222ec5",
+    "url": "/KKBOX-Sample/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/KKBOX-Sample/robots.txt"
+  }
+]);
